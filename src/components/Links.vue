aada7bf63cef50f281e0ae6692edb4e72a909f5c
Divepit <thedivepit@gmail.com>
2019-05-19 14:38:22 +0200

    inline editing with correct indexes is working for single subject

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -4,7 +4,7 @@
   <div>
     <div v-if="error">{{ error }}</div>
     <h3>Add a new link</h3>
-    <form @submit.prevent="addLink">
+    <!-- <form @submit.prevent="addLink">
       <div>
         <input autofocus autocomplete="off"
           placeholder="Link URL"
@@ -21,23 +21,21 @@
         <input autofocus autocomplete="off"
           placeholder="Field ID"
           v-model="newLink.field_id" />
+        <input autofocus autocomplete="off"
+          placeholder="Type ID"
+          v-model="newLink.type_id" />
       </div>
       <input type="submit" value="Add Link" />
-    </form>
+    </form> -->
 
-    <table>
-      <tr>
-        <th> Week </th>
-        <th v-for="type in types"> {{type}} </th>
+    <table  class="bordered">
+      <tr  class="bordered">
+        <th  class="bordered"> Week </th>
+        <th   class="bordered"v-for="type in types"> {{type.typeName}} </th>
       </tr>
-      <td>
-        <tr v-for="week in weeks">{{week}}</tr>
-      </td>
-      <td v-for="type in types">
-        <tr v-for="link in links" @click="editLink(link)">{{link.linkUrl}}</tr>
-      </td>
-      <tr>
-
+      <tr  class="bordered" v-for="week in weeks">
+        <td  class="bordered">{{week}}</td>
+        <td  class="bordered" v-for="type in types" @click="specLink(week,type,true)">{{ specLink(week,type,false) }}</td>
       </tr>
     </table>
     <table>
@@ -57,29 +55,70 @@
           </form>
         </div>
       </tr>
+      <div v-if="adding">
+        <form action="" @submit.prevent="addLink()">
+          <div>
+            <input v-model="newLink.linkUrl">
+            <input type="submit" value="Update" >
+          </div>
+        </form>
+      </div>
     </table>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
   name: 'Links',
   data () {
     return {
+      subject: 1,
+      semester: 1,
+      field: 1,
       links: [],
       newLink: [],
       error: '',
       editedLink: '',
       weeks: [1, 2, 3, 4, 5],
-      types: ['Theorie', 'Übung', 'Lösung']
+      types: [],
+      adding: false
     }
   },
   created () {
     this.$http.secured.get('/api/v1/links')
       .then(response => { this.links = response.data })
-      .catch(error => this.setError(error, 'Something went wrong'))
+      .catch(error => this.setError(error, 'Something went wrong with Links'))
+    this.$http.secured.get('/api/v1/types')
+      .then(response => { this.types = response.data })
+      .catch(error => this.setError(error, 'Something went wrong with Types'))
+  },
+  computed: {
+
   },
   methods: {
+    specLink (week, type, editor) {
+      var answer = this.$data.links.find(function (link) {
+        var speclink = link.linkWeek == week && link.type_id == type.id
+        return speclink
+      })
+      if (editor) {
+        if (!answer) {
+          this.newLink.subject_id = this.subject
+          this.newLink.semester_id = this.semester
+          this.newLink.field_id = this.field
+          this.newLink.linkWeek = week
+          this.newLink.type_id = type.id
+          this.adding = true
+        }
+        this.editedLink = answer
+      }
+      if (answer) {
+        return answer.linkUrl
+      } else {
+        return ' '
+      }
+    },
     setError (error, text) {
       this.error = (error.response && error.response.data && error.response.data.error) || text
     },
@@ -94,7 +133,8 @@ export default {
           subject_id: this.newLink.subject_id,
           semester_id: this.newLink.semester_id,
           field_id: this.newLink.field_id,
-          linkWeek: this.newLink.linkWeek
+          linkWeek: this.newLink.linkWeek,
+          type_id: this.newLink.type_id
         }
       })
 
@@ -122,7 +162,8 @@ export default {
           subject_id: link.subject_id,
           semester_id: link.semester_id,
           field_id: link.field_id,
-          linkWeek: link.linkWeek
+          linkWeek: link.linkWeek,
+          type_id: link.type_id
         }
       })
         .catch(error => this.setError(error, 'Cannot update link'))
@@ -130,3 +171,8 @@ export default {
   }
 }
 </script>
+<style media="screen">
+  .bordered {
+    border: solid !important;
+  }
+</style>
--- src/store.js
+++ src/store.js
@@ -1,20 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    secured: securedAxiosInstance,
-    plain: plainAxiosInstance
+    globalTypes: [],
+    dispError: ''
   },
   mutations: {
     signedIn () {
       return localStorage.signedIn
     },
     updateLinks (state, link) {
-      this.state.links.push(link)
+      state.links.push(link)
     }
   },
   actions: {
