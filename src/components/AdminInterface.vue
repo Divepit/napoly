8344c465f71e2ba2b7e99993ee0905c87a4ce86b
Divepit <thedivepit@gmail.com>
2019-06-30 01:33:19 +0200

    bugfix when deleting users

--- src/components/AdminInterface.vue
+++ src/components/AdminInterface.vue
@@ -2,40 +2,43 @@
   <div v-if="signer" class="">
     <Signup @pushUser="pushUser($event)"/>
     <v-container fluid grid-list-lg>
-    <v-layout  row wrap >
+    <v-layout  row wrap justify-center>
           <v-flex v-for="user in users" :key="user.id" xs12 sm12 md3>
             <v-card :color="user.role ? 'teal accent-4':'cyan darken-3'" class="white--text text-sm-left rounded">
               <v-card-title primary-title>
                 <div>
-                  <div class="headline"><v-icon medium >accessibility</v-icon> &nbsp; {{user.email}} </div>
+                  <div class="headline">
+                    <v-icon v-if="user.role == 1" large >android</v-icon>
+                    <v-icon v-else large >account_box</v-icon>
+                    &nbsp;
+                    {{user.email}}
+                  </div>
                   <br>
                   <v-divider dark />
                   <p></p>
                   <!-- <p>Created at: {{user.created_at}}</p> -->
                   <!-- <p>Updated at: {{user.updated_at}}</p> -->
                   <div style="text-align: left;">UID: {{user.id}}</div>
-                  <div style="text-align: left;">Role: {{user.role}}</div>
+                  <div style="text-align: left;">{{getRole(user)}}</div>
                 </div>
               </v-card-title>
               <v-card-actions>
-                <v-btn depressed dark color="red lighten-2" @click="dialog = !dialog">Delete</v-btn>
+                <v-btn depressed dark color="red lighten-2" @click="deleting = !deleting; currentUser = user">Delete</v-btn>
               </v-card-actions>
             </v-card>
-
-            <v-dialog v-model="dialog" persistent max-width="290">
-
-              <v-card>
-                <v-card-title class="headline">Delete User?</v-card-title>
-                <v-card-text>By confirming, the user <strong style="color: gray;">{{user.email}}</strong> will be deleted</v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="info" flat @click="dialog = false">Cancel</v-btn>
-                  <v-btn color="error" flat @click="dialog = false; removeUser(user.id)">Delete</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
           </v-flex>
     </v-layout>
+    <v-dialog v-model="deleting" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline">Delete User?</v-card-title>
+        <v-card-text>By confirming, the user <strong style="color: gray;">{{currentUser.email}}</strong> will be deleted</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="info" flat @click="deleting = false">Cancel</v-btn>
+          <v-btn color="error" flat @click="deleting = false; removeUser(currentUser.id)">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
   </v-container>
   </div>
@@ -58,7 +61,8 @@ export default {
   data () {
     return {
       users: [],
-      dialog: false
+      deleting: false,
+      currentUser: ''
     }
   },
   created () {
@@ -83,14 +87,18 @@ export default {
         })
     },
     removeUser (user) {
+      this.currentUser = ''
       this.$http.secured.delete(`/api/v1/users/${user}`)
         .then(response => {
-          this.users.splice(this.users.indexOf(user), 1)
+          this.getUsers()
         })
     },
     pushUser (user) {
       this.users.push(user)
       this.getUsers()
+    },
+    getRole (user) {
+      return user.role === 1 ? 'Role: Admin' : `Role: ${user.role}`
     }
   }
 }
