e27c3ea5ac270c37058b83385c787a4fc3543a2a
Divepit <thedivepit@gmail.com>
2019-07-21 12:55:12 +0200

    bugfix in adding buttons

--- src/components/SubjectButtons.vue
+++ src/components/SubjectButtons.vue
@@ -104,6 +104,7 @@ export default {
           }
         })
           .then(response => {
+            this.buttons.push(response.Data)
             this.getButtons()
             this.stopEditing()
           })
--- src/components/SubjectInfos.vue
+++ src/components/SubjectInfos.vue
@@ -109,6 +109,7 @@ export default {
       })
         .then(response => {
           this.infos.push(response.data)
+          this.getInfos()
         })
     },
     editInfo (info) {
