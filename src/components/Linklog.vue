79f95a9142d2688059d939e59c658678d72156ea
Divepit <thedivepit@gmail.com>
2019-09-18 14:39:36 +0200

    linklog refined (no editing capabilities)

--- src/components/Linklog.vue
+++ src/components/Linklog.vue
@@ -6,9 +6,14 @@
             :headers="headers"
             :items="links"
             class="elevation-1"
+            disable-initial-sort
           >
             <template v-slot:items="props">
-              <td>{{ props.item.linkUrl }}</td>
+              <td>{{ props.item.id }}</td>
+              <td>{{ subjects[props.item.subject_id].subjectName }}</td>
+              <td>{{ types[props.item.type_id].typeName }}</td>
+              <td>{{ props.item.linkWeek }}</td>
+              <td>{{ props.item.created_at }}</td>
               <td>{{ props.item.updated_at }}</td>
             </template>
           </v-data-table>
@@ -21,22 +26,23 @@
 export default {
   created () {
     this.getLinks()
+    this.getSubjects()
+    this.getTypes()
   },
   data () {
     return {
       links: [],
+      subjects: [],
+      types: [],
       headers: [
-          {
-            text: 'Url',
-            sortable: false,
-            value: 'linkUrl'
-          },
-          {
-            text: 'Updated',
-            sortable: false,
-            value: 'updated_at'
-          },
-        ]
+        { text: 'Link ID', value: 'id' },
+        { text: 'Subject', value: 'subject_id' },
+        { text: 'Type', value: 'type_id' },
+        { text: 'Week', value: 'linkWeek' },
+        { text: 'Created', sortable: false, value: 'created_at' },
+        { text: 'Updated', sortable: false, value: 'updated_at' }
+
+      ]
     }
   },
   methods: {
@@ -45,6 +51,25 @@ export default {
         .then(response => {
           if (response.data.length) {
             this.links = response.data
+            this.links = this.links.reverse()
+          }
+        })
+    },
+    getSubjects () {
+      this.$http.secured.get('/api/v1/subjects')
+        .then(response => {
+          if (response.data.length) {
+            this.subjects = response.data
+            this.subjects = this.subjects.reverse()
+          }
+        })
+    },
+    getTypes () {
+      this.$http.secured.get('/api/v1/types')
+        .then(response => {
+          if (response.data.length) {
+            this.types = response.data
+            this.types = this.types.reverse()
           }
         })
     }
