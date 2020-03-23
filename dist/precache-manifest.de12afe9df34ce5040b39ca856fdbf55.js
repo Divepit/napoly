7d8a9508185b57ef43351e387f4345b0e6ba5a67
Divepit <thedivepit@gmail.com>
2020-03-23 19:34:58 +0100

    forgot to remove dist folder from gitignore

---
+++ dist/precache-manifest.de12afe9df34ce5040b39ca856fdbf55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3db25df3e65c14ab6e8",
+    "url": "/css/chunk-vendors.1d780981.css"
+  },
+  {
+    "revision": "7cf0b8ed50dc15fad668c4468e9fc6ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e138407542fda641114b",
+    "url": "/js/app.b0486555.js"
+  },
+  {
+    "revision": "b3db25df3e65c14ab6e8",
+    "url": "/js/chunk-vendors.2a2c6f7b.js"
+  },
+  {
+    "revision": "42c6dae9798891cd7c8fe13198bdd83e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
